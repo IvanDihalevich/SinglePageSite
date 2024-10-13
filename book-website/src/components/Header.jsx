0896dd1515e14d9c2ffc54bb4bf356ec7c6d2de4
Ivan Dihalevych <143941559+IvanDihalevich@users.noreply.github.com>
2024-10-13 03:00:27 +0300

    update header

--- book-website/src/components/Header.jsx
+++ book-website/src/components/Header.jsx
@@ -1,15 +1,55 @@
-// src/components/Header.jsx
-import React from 'react';
+import React from "react";
+import FaYoutube from "../icons/FaYoutube.png";
+import FaTiktok from "../icons/FaTiktok.png";
+import FaTelegram from "../icons/FaTelegram.png";
+import FaInstagram from "../icons/FaInstagram.png";
+import "../css/Header.css";
 
 const Header = () => (
-    <header>
-        <h1>Книги про Гаррі Поттера</h1>
-        <nav>
-            <a href="#about">Про книгу</a>
-            <a href="#author">Про автора</a>
-            <a href="#reviews">Відгуки</a>
-        </nav>
-    </header>
+    <header className="header">
+  <div className="top-row">
+    <div className="left-section">
+      <div className="social-icons">
+        <a href="#" aria-label="Youtube">
+          <img src={FaYoutube} alt="Youtube" />
+        </a>
+        <a href="#" aria-label="Tiktok">
+          <img src={FaTiktok} alt="Tiktok" />
+        </a>
+        <a href="#" aria-label="Telegram">
+          <img src={FaTelegram} alt="Telegram" />
+        </a>
+        <a href="#" aria-label="Instagram">
+          <img src={FaInstagram} alt="Instagram" />
+        </a>
+      </div>
+    </div>
+
+    <div className="center-section">
+      <h1 className="logo">Harry Potter</h1>
+    </div>
+
+    <div className="right-section">
+      <input type="text" placeholder="Search" className="search-bar" />
+      <button className="login">LOG IN</button>
+      <button className="signup">SIGN UP</button>
+    </div>
+  </div>
+
+  <div className="bottom-row">
+    <nav className="nav">
+      <a href="#">КНИГИ</a>
+      <a href="#">ВІДЕО ПРО ГАРІ ПОТТЕРА</a>
+      <a href="#">ЩОСЬ ТАМ</a>
+      <a href="#">ПЕРСОНАЖІ</a>
+      <a href="#">ЩОСЬ ТАМ</a>
+      <a href="#">J.K. Rowling</a>
+      <a href="#">АВТОР САЙТУ</a>
+    </nav>
+  </div>
+</header>
+
+
 );
 
 export default Header;
